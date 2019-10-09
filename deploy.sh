4b4f6fbab611756b3b725988e1e6992d8303ea42
Civan Özseyhan <drcivan@Civans-MacBook-Pro-2.local>
2019-10-09 18:26:32 +0300

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get install nodejs npm git
+npm install pm2@latest -g
+cd ~
+rm -rf puul-print-server
+git clone https://github.com/ebias/puul-print-server.git
+cd puul-print-server
+npm install
+sudo pm2 start index.js
+sudo pm2 startup
+pm2 save
